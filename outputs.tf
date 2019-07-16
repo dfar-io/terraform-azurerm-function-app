63bf337a21901424bcc123c960baf170f06f2d01
Dave Farinelli <d@dfar.io>
2019-07-16 09:44:17 -0400

    Adding storage account PK as output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "storage_account_name" {
   value       = "${azurerm_storage_account.sa.name}"
 }
 
+output "storage_account_primary_key" {
+  description = "Primary key of the Storage Account created."
+  value       = "${azurerm_storage_account.sa.storage_account_primary_key}"
+}
+
 output "storage_account_primary_connection_string" {
   description = "Primary connection string for the Storage Account created."
   value       = "${azurerm_storage_account.sa.primary_connection_string}"
