e19ee7107b14f5b5134d9ea17111172cec3d36ed
Dave Farinelli <d@dfar.io>
2019-07-16 09:02:21 -0400

    Added SA name to outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "storage_account_name" {
+  description = "Name of the Storage Account created."
+  value       = "${azurerm_storage_account.sa.name}"
+}
+
 output "storage_account_primary_connection_string" {
   description = "Primary connection string for the Storage Account created."
   value       = "${azurerm_storage_account.sa.primary_connection_string}"
