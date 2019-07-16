d1922bc7dd55375b85292f496c055a1534e520cc
Dave Farinelli <d@dfar.io>
2019-07-16 09:49:48 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "storage_account_name" {
 
 output "storage_account_primary_access_key" {
   description = "Primary key of the Storage Account created."
-  value       = "${azurerm_storage_account.sa.storage_account_primary_access_key}"
+  value       = "${azurerm_storage_account.sa.primary_access_key}"
 }
 
 output "storage_account_primary_connection_string" {
