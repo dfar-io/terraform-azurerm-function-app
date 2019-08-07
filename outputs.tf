7879459f84f7b3b8a5e6ca5a9fb604e8b2c416b3
Dave Farinelli <d@dfar.io>
2019-08-07 10:54:37 -0400

    Added FA possible outbound IP address output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "storage_account_primary_connection_string" {
   description = "Primary connection string for the Storage Account created."
   value       = "${azurerm_storage_account.sa.primary_connection_string}"
 }
+
+output "possible_outbound_ip_addresses" {
+  description = "The possible IP addresses of the Function App."
+  value       = "${azurerm_function_app.fa.possible_outbound_ip_addresses}"
+}
