7d1474686a0b0cf16b3f7777022c58fb5ab622ab
Dave Farinelli <d@dfar.io>
2019-12-09 10:22:22 -0500

    Removed deprecated interpolation statements

--- main.tf
+++ main.tf
@@ -5,40 +5,40 @@ terraform {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                      = "${var.storage_account_name}"
-  resource_group_name       = "${var.rg_name}"
-  location                  = "${var.rg_location}"
+  name                      = var.storage_account_name
+  resource_group_name       = var.rg_name
+  location                  = var.rg_location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
-  account_kind              = "${var.storage_account_kind}"
-  enable_https_traffic_only = "${var.storage_account_enable_https_traffic_only}"
+  account_kind              = var.storage_account_kind
+  enable_https_traffic_only = var.storage_account_enable_https_traffic_only
 }
 
 resource "azurerm_app_service_plan" "asp_fa" {
-  name                = "${var.function_app_plan_name}"
-  resource_group_name = "${var.rg_name}"
-  location            = "${var.rg_location}"
-  kind                = "${var.tier == "Dynamic" && var.size == "Y1" ? "FunctionApp" : "Windows"}"
+  name                = var.function_app_plan_name
+  resource_group_name = var.rg_name
+  location            = var.rg_location
+  kind                = var.tier == "Dynamic" && var.size == "Y1" ? "FunctionApp" : "Windows"
 
   sku {
-    tier = "${var.tier}"
-    size = "${var.size}"
+    tier = var.tier
+    size = var.size
   }
 }
 
 resource "azurerm_function_app" "fa" {
-  name                      = "${var.function_app_name}"
-  resource_group_name       = "${var.rg_name}"
-  location                  = "${var.rg_location}"
-  app_service_plan_id       = "${azurerm_app_service_plan.asp_fa.id}"
-  storage_connection_string = "${azurerm_storage_account.sa.primary_connection_string}"
+  name                      = var.function_app_name
+  resource_group_name       = var.rg_name
+  location                  = var.rg_location
+  app_service_plan_id       = azurerm_app_service_plan.asp_fa.id
+  storage_connection_string = azurerm_storage_account.sa.primary_connection_string
   version                   = "~2"
-  app_settings              = "${var.app_settings}"
-  https_only                = "${var.https_only}"
+  app_settings              = var.app_settings
+  https_only                = var.https_only
   site_config {
-    always_on = "${var.always_on}"
+    always_on = var.always_on
     cors {
-      allowed_origins = "${var.cors_allowed_origins}"
+      allowed_origins = var.cors_allowed_origins
     }
   }
 }
