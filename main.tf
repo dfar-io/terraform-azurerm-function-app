092d15bd1a5384658e82cf0ace23fe83ff69d9dd
Dave Farinelli <d@dfar.io>
2020-06-29 14:38:56 -0400

    Can link to existing plan & storage account

--- main.tf
+++ main.tf
@@ -5,17 +5,19 @@ terraform {
 }
 
 resource "azurerm_storage_account" "sa" {
+  count                     = var.storage_account_name != "" ? 1 : 0
   name                      = var.storage_account_name
   resource_group_name       = var.rg_name
   location                  = var.rg_location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
   account_kind              = var.storage_account_kind
-  enable_https_traffic_only = var.storage_account_enable_https_traffic_only
+  enable_https_traffic_only = !var.allow_storage_account_http_traffic
 }
 
 resource "azurerm_app_service_plan" "asp_fa" {
-  name                = var.function_app_plan_name
+  count               = var.app_service_plan_name != "" ? 1 : 0
+  name                = var.app_service_plan_name
   resource_group_name = var.rg_name
   location            = var.rg_location
   kind                = var.tier == "Dynamic" && var.size == "Y1" ? "FunctionApp" : "Windows"
@@ -30,11 +32,11 @@ resource "azurerm_function_app" "fa" {
   name                      = var.function_app_name
   resource_group_name       = var.rg_name
   location                  = var.rg_location
-  app_service_plan_id       = azurerm_app_service_plan.asp_fa.id
-  storage_connection_string = azurerm_storage_account.sa.primary_connection_string
-  version                   = "~2"
+  app_service_plan_id       = var.app_service_plan_id != "" ? var.app_service_plan_id : azurerm_app_service_plan.asp_fa[0].id
+  storage_connection_string = var.storage_account_connection_string != "" ? var.storage_account_connection_string : azurerm_storage_account.sa[0].primary_connection_string
+  version                   = var.runtime_version
   app_settings              = var.app_settings
-  https_only                = var.https_only
+  https_only                = !var.allow_http
   site_config {
     always_on = var.always_on
     cors {
--- variables.tf
+++ variables.tf
@@ -1,35 +1,31 @@
+# Required
 variable "rg_name" {
   description = "The name of the Resource Group the Function App will be contained in."
 }
 variable "rg_location" {
-  description = "The name of the Resource Group the Function App will be contained in."
+  description = "The location of the Resource Group the Function App will be contained in."
 }
-variable "storage_account_name" {
-  description = "The Storage Account name for Function App."
+variable "function_app_name" {
+  description = "The Function App Name."
 }
+
+# Optional
 variable "storage_account_kind" {
   description = "The Storage Account type."
   default     = "Storage"
 }
-variable "storage_account_enable_https_traffic_only" {
-  description = "If the created Storage Account should only allow HTTPS connections."
-  default     = "false"
-}
-variable "function_app_plan_name" {
-  description = "The Function App Service Plan."
-}
-variable "function_app_name" {
-  description = "The Function App Name."
+variable "allow_storage_account_http_traffic" {
+  description = "Allows for the Storage Account created to accept HTTP traffic"
+  default     = false
 }
 variable "app_settings" {
   description = "Function App settings."
   default     = {}
 }
-variable "https_only" {
-  description = "If the function app should force HTTPS."
+variable "allow_http" {
+  description = "Allows for the Function App to accept HTTP connections."
   default     = false
 }
-
 variable "always_on" {
   description = "Whether the Function App should always be on (only applies to dedicated App Service Plans)."
   default     = false
@@ -46,4 +42,24 @@ variable "cors_allowed_origins" {
   description = "Allowed origins (CORS) for the Function App."
   type        = list(string)
   default     = []
+}
+variable "runtime_version" {
+  description = "The runtime version of the Function App."
+  default     = "~3"
+}
+variable "app_service_plan_id" {
+  description = "ID of an existing App Service plan (allows for multiple function apps to use a single plan)."
+  default     = ""
+}
+variable "app_service_plan_name" {
+  description = "The name of the App Service plan to create."
+  default     = ""
+}
+variable "storage_account_connection_string" {
+  description = "Connection string of an existing Storage Account (allows for multiple function apps to use a single storage account)."
+  default     = ""
+}
+variable "storage_account_name" {
+  description = "Storage Account name."
+  default     = ""
 }
